689f3b23ea1e136cecd666069fc9e01ed13cfc44
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2024-05-16 05:10:21 +0100

    latest

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,15 +1,20 @@
 import os
 
 from .base import *
+import dj_database_url
 ALLOWED_HOSTS = ["regio-backend.onrender.com"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(config("DB_URL")),
 }
 # media upload and accessing
 MEDIA_URL = "/media/"
