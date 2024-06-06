d598367e248ad0a1949c1ac2eb4ee9dbad28b0c9
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2024-06-06 13:18:13 +0100

    added the notification model and update the requirements.txt

--- apps/notification/models.py
+++ apps/notification/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Notification(models.Model):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "notification"
+        verbose_name = "notification"
+        verbose_name_plural = "notifications"
+
+    def __str__(self):
+        return self.title
