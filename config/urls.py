bdf8cbeac9023c5959fc9efbb9cd7b092eb70ad2
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2024-06-19 16:23:08 +0100

    added img

--- config/urls.py
+++ config/urls.py
@@ -53,5 +53,4 @@ urlpatterns = [
     path(f"{api_version}/", include("common.urls", namespace="common")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
