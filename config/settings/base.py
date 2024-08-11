842f89dbfa9c9c1257c4ef97864573abdab16db8
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2024-08-11 17:07:04 +0100

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,8 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # media upload and accessing
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "https://myregio.farm/media/"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
